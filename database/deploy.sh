085b7fdb6030400067e8b5a2954bde66cee1616f
bart-kosmala <bartoshmen@gmail.com>
2020-10-28 14:54:45 +0100

    working on db script

--- database/deploy.sh
+++ database/deploy.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/bash
 
+if [ -z "$(which psql)" ]; then
+    echo psql not found, installing...
+
+    sudo apt-get -y install postgresql-12 postgresql-client-12
+
+    if [ ! -z "$(which psql)" ]; then
+        echo instalation successful, configuring database
+    fi
+fi
+
