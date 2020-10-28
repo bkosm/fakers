54935eea7ebb9c6db1abbfaeec32d2677c6754bf
bart-kosmala <bartoshmen@gmail.com>
2020-10-28 19:01:52 +0100

    masno fest pionier hostuje

--- database/deploy.sh
+++ database/deploy.sh
@@ -3,6 +3,14 @@
 if [ -z "$(which psql)" ]; then
     echo psql not found, installing...	
 
+  # Add postgres repo to the package manager
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+  wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+  sudo apt-get update
+
+  #Install pg and tools
 	sudo apt-get -y install postgresql-13
 	sudo pg_createcluster 13 main
 	sudo pg_ctlcluster 13 main start
