2229c83f0741453407b3a47f8424b35753c54921
bart-kosmala <bartoshmen@gmail.com>
2020-10-28 14:06:07 +0100

    db rework

--- database/tables.sql
+++ database/tables.sql
@@ -11,15 +11,14 @@ CREATE TABLE IF NOT EXISTS address (
 CREATE TABLE IF NOT EXISTS contact (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 
-    email VARCHAR(320) NOT NULL UNIQUE,
+    email VARCHAR(320) NOT NULL,
     phone_number VARCHAR(13),
+
+    UNIQUE(email, phone_number)
 );
 
 CREATE TABLE IF NOT EXISTS person (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-
-    address_id INT REFERENCES address(id) NOT NULL,
-    contact_id INT REFERENCES contact(id) NOT NULL UNIQUE,
 
     pesel CHAR(11) NOT NULL UNIQUE,
     
@@ -29,3 +28,38 @@ CREATE TABLE IF NOT EXISTS person (
 
     birth_date DATE NOT NULL
 );
+
+CREATE INDEX ON person(email);
+CREATE INDEX ON person(birth_date);
+CREATE INDEX ON person(pesel);
+CREATE INDEX ON person(last_name);
+
+CREATE TABLE IF NOT EXISTS deceased_person (
+    person_id BIGINT PRIMARY KEY REFERENCES person(id),
+
+    date_of_death DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX ON deceased_person(date_of_death);
+
+CREATE TABLE IF NOT EXISTS person_address (
+    person_id BIGINT REFERENCES person(id),
+    address_id BIGINT REFERENCES address(id),
+
+    PRIMARY KEY (person_id, address_id),
+
+    assigned DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE INDEX ON person_address(assigned);
+
+CREATE TABLE IF NOT EXISTS person_contact (
+    person_id BIGINT REFERENCES person(id),
+    contact_id BIGINT REFERENCES contact(id) UNIQUE,
+
+    PRIMARY KEY (person_id, contact_id),
+
+    assigned DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX ON person_contact(assigned);
