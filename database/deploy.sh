86aeb1df3918a65ef6c40cfcb55043207915c77a
bart-kosmala <bartoshmen@gmail.com>
2020-10-28 15:59:16 +0100

    installation working

--- database/deploy.sh
+++ database/deploy.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 
 if [ -z "$(which psql)" ]; then
-    echo psql not found, installing...
+    echo psql not found, installing...	
 
-    sudo apt-get -y install postgresql-12 postgresql-client-12
+	sudo apt-get -y install postgresql-13
+	sudo pg_createcluster 13 main
+	sudo pg_ctlcluster 13 main start
+
+
 
     if [ ! -z "$(which psql)" ]; then
         echo instalation successful, configuring database
     fi
 fi
+
+sudo -u postgres psql
 
