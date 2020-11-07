09e62df2565e7b4982c7931acba389a451ac471c
Hubert <hubert.kniola@student.put.poznan.pl>
2020-11-07 20:02:10 +0100

    graphql

--- client/fakerweb/src/App.js
+++ client/fakerweb/src/App.js
@@ -4,24 +4,28 @@ import "./App.css";
 import Home from "./Pages/Home";
 import Info from "./Pages/Info";
 import Graph from "./Pages/Graph";
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/Info">
-            <Info></Info>
-          </Route>
-          <Route path="/Graph">
-            <Graph></Graph>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <GraphiQL
+      fetcher={async (graphQLParams) => {
+        const data = await fetch(
+          "https://swapi-graphql.netlify.app/.netlify/functions/index",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(graphQLParams),
+            credentials: "same-origin",
+          }
+        );
+        return data.json().catch(() => data.text());
+      }}
+    />
   );
 }
 
--- client/fakerweb/src/Pages/Home.js
+++ client/fakerweb/src/Pages/Home.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import QueryInputWindow from '../Components/QueryInputWindow';
-import NavBar from '../Components/NavBar';
-import NavItem from '../Components/NavItem';
-import { ResultDisplayWindow } from '../Components/ResultDisplayWindow';
-import Logo from '../Assets/logo512frs.png';
+import QueryInputWindow from "../Components/QueryInputWindow";
+import NavBar from "../Components/NavBar";
+import NavItem from "../Components/NavItem";
+import { ResultDisplayWindow } from "../Components/ResultDisplayWindow";
+import Logo from "../Assets/logo512frs.png";
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.min.css";
 
 function Home() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [isNavBarOpen, setIsNavBarOpen] = useState(false);
   const [isRequestInProgress, setIsRequestInProgress] = useState(false);
   return (
@@ -15,39 +17,33 @@ function Home() {
       <NavBar
         style={{
           opacity: isNavBarOpen ? 1 : 0,
-          transition: isNavBarOpen ? 0 : '1s',
+          transition: isNavBarOpen ? 0 : "1s",
         }}
       >
         <NavItem
-          icon={<img className='navitemicon' src={Logo} alt='logo' />}
-          href='https://github.com/bart-kosmala/fakers'
+          icon={<img className="navitemicon" src={Logo} alt="logo" />}
+          href="https://github.com/bart-kosmala/fakers"
         />
-        <NavItem icon={<img className='navitemicon' src={Logo} alt='logo' />} />
-        <NavItem icon={<img className='navitemicon' src={Logo} alt='logo' />} />
+        <NavItem icon={<img className="navitemicon" src={Logo} alt="logo" />} />
+        <NavItem icon={<img className="navitemicon" src={Logo} alt="logo" />} />
       </NavBar>
-      <div className='row'>
-        <div className='inputColumn'>
-          <button
-            className='Logo'
-            style={{ top: isNavBarOpen ? '85%' : 0, transition: '1s' }}
-            onClick={() => setIsNavBarOpen(!isNavBarOpen)}
-          >
-            <img className='LogoImage' src={Logo} alt='logo' />
-          </button>
-
-          <QueryInputWindow header={'jakis tytul'} onChange={setText} />
-          <button
-            className='inputButton'
-            onClick={() => {
-              setIsRequestInProgress(true);
-              setTimeout(() => setIsRequestInProgress(false), 2000);
-            }}
-          >
-            {isRequestInProgress ? 'Loading...' : 'Execute'}
-          </button>
-        </div>
-        <ResultDisplayWindow result={text} />
-      </div>
+      <GraphiQL
+        fetcher={async (graphQLParams) => {
+          const data = await fetch(
+            "https://swapi-graphql.netlify.app/.netlify/functions/index",
+            {
+              method: "POST",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(graphQLParams),
+              credentials: "same-origin",
+            }
+          );
+          return data.json().catch(() => data.text());
+        }}
+      />
     </>
   );
 }
