0c3d327b652dd588fa9a3e219739128e257f8e60
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-11-07 21:19:40 +0100

    Add routing to App component

--- client/fakerweb/src/App.js
+++ client/fakerweb/src/App.js
@@ -1,31 +1,27 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import "./App.css";
-import Home from "./Pages/Home";
-import Info from "./Pages/Info";
-import Graph from "./Pages/Graph";
-import GraphiQL from "graphiql";
-import "graphiql/graphiql.min.css";
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Info from './Pages/Info';
+import Graph from './Pages/Graph';
 
 function App() {
   return (
-    <GraphiQL
-      fetcher={async (graphQLParams) => {
-        const data = await fetch(
-          "https://swapi-graphql.netlify.app/.netlify/functions/index",
-          {
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(graphQLParams),
-            credentials: "same-origin",
-          }
-        );
-        return data.json().catch(() => data.text());
-      }}
-    />
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/Info'>
+            <Info></Info>
+          </Route>
+          <Route path='/Graph'>
+            <Graph></Graph>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
