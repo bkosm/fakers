573b18c926bda1c34ecb91992dc9760885f8dc32
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-11-07 21:12:30 +0100

    Resolve rebase conflicts

--- client/fakerweb/src/Pages/Home.js
+++ client/fakerweb/src/Pages/Home.js
@@ -1,44 +1,47 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import QueryInputWindow from "../Components/QueryInputWindow";
-import NavBar from "../Components/NavBar";
-import NavItem from "../Components/NavItem";
-import { ResultDisplayWindow } from "../Components/ResultDisplayWindow";
-import Logo from "../Assets/logo512frs.png";
-import GraphiQL from "graphiql";
-import "graphiql/graphiql.min.css";
+import NavBar from '../Components/NavBar';
+import NavItem from '../Components/NavItem';
+import Logo from '../Assets/logo512frs.png';
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.min.css';
 
 function Home() {
-  const [text, setText] = useState("");
   const [isNavBarOpen, setIsNavBarOpen] = useState(false);
-  const [isRequestInProgress, setIsRequestInProgress] = useState(false);
   return (
     <>
+      <button
+        className='Logo'
+        style={{ top: isNavBarOpen ? '85%' : 0, transition: '1s' }}
+        onClick={() => setIsNavBarOpen(!isNavBarOpen)}
+      >
+        <img className='LogoImage' src={Logo} alt='logo' />
+      </button>
       <NavBar
         style={{
           opacity: isNavBarOpen ? 1 : 0,
-          transition: isNavBarOpen ? 0 : "1s",
+          transition: isNavBarOpen ? 0 : '1s',
         }}
       >
         <NavItem
-          icon={<img className="navitemicon" src={Logo} alt="logo" />}
-          href="https://github.com/bart-kosmala/fakers"
+          icon={<img className='navitemicon' src={Logo} alt='logo' />}
+          href='https://github.com/bart-kosmala/fakers'
         />
-        <NavItem icon={<img className="navitemicon" src={Logo} alt="logo" />} />
-        <NavItem icon={<img className="navitemicon" src={Logo} alt="logo" />} />
+        <NavItem icon={<img className='navitemicon' src={Logo} alt='logo' />} />
+        <NavItem icon={<img className='navitemicon' src={Logo} alt='logo' />} />
       </NavBar>
       <GraphiQL
         fetcher={async (graphQLParams) => {
           const data = await fetch(
-            "https://swapi-graphql.netlify.app/.netlify/functions/index",
+            'https://swapi-graphql.netlify.app/.netlify/functions/index',
             {
-              method: "POST",
+              method: 'POST',
               headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
               },
               body: JSON.stringify(graphQLParams),
-              credentials: "same-origin",
+              credentials: 'same-origin',
             }
           );
           return data.json().catch(() => data.text());
