b85d3aeeca1fa64a0dd9fd04d9e1b150e7f25716
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-11-07 18:06:02 +0100

    Fix navbar styling

--- client/fakerweb/src/Pages/Home.js
+++ client/fakerweb/src/Pages/Home.js
@@ -1,62 +1,54 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import LinksButtons from "../Components/LinksButtons";
-import QueryInputWindow from "../Components/QueryInputWindow";
-import NavBar from "../Components/NavBar";
-import NavItem from "../Components/NavItem";
-import { ResultDisplayWindow } from "../Components/ResultDisplayWindow";
-import Logo from "../Assets/logo512frs.png";
+import QueryInputWindow from '../Components/QueryInputWindow';
+import NavBar from '../Components/NavBar';
+import NavItem from '../Components/NavItem';
+import { ResultDisplayWindow } from '../Components/ResultDisplayWindow';
+import Logo from '../Assets/logo512frs.png';
 
-function Home(props) {
-  const [text, setText] = useState("");
+function Home() {
+  const [text, setText] = useState('');
   const [isNavBarOpen, setIsNavBarOpen] = useState(false);
   const [isRequestInProgress, setIsRequestInProgress] = useState(false);
   return (
-    <div className="row">
-      <div className="inputColumn">
-        <button
-          className="Logo"
-          style={{ top: isNavBarOpen ? "85%" : 0, transition: "1s" }}
-          onClick={() => setIsNavBarOpen(!isNavBarOpen)}
-        >
-          <img className="LogoImage" src={Logo} alt="logo" />
-        </button>
-        <NavBar
-          style={{
-            opacity: isNavBarOpen ? 1 : 0,
-            transition: isNavBarOpen ? 0 : "1s",
-          }}
-        >
-          <li>
-            <NavItem
-              icon={<img className="navitemicon" src={Logo} alt="logo" />}
-              href="https://github.com/bart-kosmala/fakers"
-            />
-          </li>
-          <li>
-            <NavItem
-              icon={<img className="navitemicon" src={Logo} alt="logo" />}
-            />
-          </li>
-          <li>
-            <NavItem
-              icon={<img className="navitemicon" src={Logo} alt="logo" />}
-            />
-          </li>
-        </NavBar>
-        <QueryInputWindow header={"jakis tytul"} onChange={setText} />
-        <button
-          className="inputButton"
-          onClick={() => {
-            setIsRequestInProgress(true);
-            setTimeout(() => setIsRequestInProgress(false), 2000);
-          }}
-        >
-          {isRequestInProgress ? "Loading..." : "Execute"}
-        </button>
+    <>
+      <NavBar
+        style={{
+          opacity: isNavBarOpen ? 1 : 0,
+          transition: isNavBarOpen ? 0 : '1s',
+        }}
+      >
+        <NavItem
+          icon={<img className='navitemicon' src={Logo} alt='logo' />}
+          href='https://github.com/bart-kosmala/fakers'
+        />
+        <NavItem icon={<img className='navitemicon' src={Logo} alt='logo' />} />
+        <NavItem icon={<img className='navitemicon' src={Logo} alt='logo' />} />
+      </NavBar>
+      <div className='row'>
+        <div className='inputColumn'>
+          <button
+            className='Logo'
+            style={{ top: isNavBarOpen ? '85%' : 0, transition: '1s' }}
+            onClick={() => setIsNavBarOpen(!isNavBarOpen)}
+          >
+            <img className='LogoImage' src={Logo} alt='logo' />
+          </button>
+
+          <QueryInputWindow header={'jakis tytul'} onChange={setText} />
+          <button
+            className='inputButton'
+            onClick={() => {
+              setIsRequestInProgress(true);
+              setTimeout(() => setIsRequestInProgress(false), 2000);
+            }}
+          >
+            {isRequestInProgress ? 'Loading...' : 'Execute'}
+          </button>
+        </div>
+        <ResultDisplayWindow result={text} />
       </div>
-      <ResultDisplayWindow result={text} />
-    </div>
+    </>
   );
 }
 
