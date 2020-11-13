1723db1b1bd118925f4fd343f1599e6816e9c347
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-13 21:20:14 +0100

    random number generator

---
+++ generator/PeopleGenerator/GeneratorsClass/RandomNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.Security.Cryptography;
+
+namespace GeneratorsClass
+{
+    public static class RandomNumber
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        public static int Draw(int minValue, int maxValue)
+        {
+            byte[] randomNumber = new byte[1];
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d)- 0.00000000001d);
+            int range = maxValue - minValue + 1;
+
+            double randomValueRange = Math.Floor(multiplier * range);
+            return (int)(minValue + randomValueRange);
+        }
+
+    }
+}
