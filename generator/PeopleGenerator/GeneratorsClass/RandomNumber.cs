4b76cf7da74e8a8a3c6b03a9705a0b556dc70a27
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-14 15:37:15 +0100

    function overloading

--- generator/PeopleGenerator/GeneratorsClass/RandomNumber.cs
+++ generator/PeopleGenerator/GeneratorsClass/RandomNumber.cs
@@ -21,6 +21,19 @@ namespace GeneratorsClass
             double randomValueRange = Math.Floor(multiplier * range);
             return (int)(minValue + randomValueRange);
         }
+        public static int Draw()
+        {
+            byte[] randomNumber = new byte[1];
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            int range = (int.MaxValue/2)-1 - 0 + 1;
+
+            double randomValueRange = Math.Floor(multiplier * range);
+            return (int)(0 + randomValueRange);
+        }
 
     }
 }
