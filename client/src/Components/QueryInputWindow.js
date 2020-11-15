9ec97dc8b4a0d25f866b5e6985d6b2078ea1fdf0
Roch Goszczyński <roch.goszczynski@gmail.com>
2020-11-15 15:56:17 +0100

    Add dynamic server address

--- client/src/Components/QueryInputWindow.js
+++ client/src/Components/QueryInputWindow.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function QueryInputWindow(props) {
+  const [input, setInput] = useState('');
+  const onSubmit = () => props.onSubmit(input);
+
   return (
-    <div className="windowInput">
-      <input className="connectButton" type="submit" value="Connect" />
-      <textarea placeholder="GraphQL API server address" value={props.name} />
+    <div className='windowInput'>
+      <input
+        className='connectButton'
+        type='submit'
+        value='Connect'
+        onClick={onSubmit}
+      />
+      <textarea
+        placeholder='GraphQL API server address'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
   );
 }
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import GraphiQL from "graphiql";
-import "graphiql/graphiql.min.css";
-import QueryInputWindow from "../Components/QueryInputWindow";
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.min.css';
+import QueryInputWindow from '../Components/QueryInputWindow';
 
 function Home() {
+  const [serverUrl, setServerUrl] = useState('');
+
   return (
     <>
       <GraphiQL
         fetcher={async (graphQLParams) => {
-          const data = await fetch(
-            "https://swapi-graphql.netlify.app/.netlify/functions/index",
-            {
-              method: "POST",
-              headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(graphQLParams),
-              credentials: "same-origin",
-            }
-          );
+          const data = await fetch(serverUrl, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(graphQLParams),
+            credentials: 'same-origin',
+          });
           return data.json().catch(() => data.text());
         }}
       />
-      <QueryInputWindow></QueryInputWindow>
+      <QueryInputWindow
+        value={serverUrl}
+        onSubmit={(value) => setServerUrl(value)}
+      />
     </>
   );
 }
