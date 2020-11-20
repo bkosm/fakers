9a1efe9c1f0a3854791b989ca3500191dfadeb13
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-20 11:52:07 +0100

    random method separated into a separate library

---
+++ generator/PeopleGenerator/RandomClass/RandomNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using System.Security.Cryptography;
+
+namespace RandomClass
+{
+    public static class RandomNumber
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        public static int Draw(int minValue, int maxValue)
+        {
+            byte[] randomNumber = new byte[1];
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d)- 0.00000000001d);
+            int range = maxValue - minValue + 1;
+
+            double randomValueRange = Math.Floor(multiplier * range);
+            return (int)(minValue + randomValueRange);
+        }
+        public static int Draw()
+        {
+            byte[] randomNumber = new byte[1];
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            int range = (int.MaxValue/2)-1 - 0 + 1;
+
+            double randomValueRange = Math.Floor(multiplier * range);
+            return (int)(0 + randomValueRange);
+        }
+
+    }
+}
