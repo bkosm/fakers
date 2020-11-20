19a8d619e4411bd67113b0688ca5aa75eb07a8ed
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-20 11:50:53 +0100

    add death metode

---
+++ generator/PeopleGenerator/GeneratorsClass/DeathDate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneratorsClass
+{
+    class DeathDate
+    {
+        DateTime _date;
+
+        DeathDate(DateTime birthDate){ _date = new Date(birthDate).getDate; }
+    }
+}
