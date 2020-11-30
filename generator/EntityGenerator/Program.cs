c1cb83e0aaa668ca603f5ad88911f292c7dd6b58
bart-kosmala <bartoshmen@gmail.com>
2020-11-30 14:39:51 +0100

    context init

---
+++ generator/EntityGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace EntityGenerator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var context = new PeopleContext();
+
+            foreach (var person in context.People)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
---
+++ generator/EntityGenerator/model/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityGenerator.model
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            PersonAddresses = new HashSet<PersonAddress>();
+            PersonContacts = new HashSet<PersonContact>();
+        }
+
+        public long Id { get; set; }
+        public string Pesel { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public char Sex { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public virtual DeceasedPerson DeceasedPerson { get; set; }
+        public virtual ICollection<PersonAddress> PersonAddresses { get; set; }
+        public virtual ICollection<PersonContact> PersonContacts { get; set; }
+        
+        public override string ToString() => $"{Id} {Pesel} {FirstName} {SecondName} {LastName} {Sex} {BirthDate}";
+    }
+}
