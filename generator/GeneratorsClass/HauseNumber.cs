ef7f3c5f83acc1bee903ed09b60682594923626d
Mateusz Kuźniak <mateokuz@gmail.com>
2020-12-01 14:50:36 +0100

    apartament number generator

---
+++ generator/GeneratorsClass/HauseNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RandomClass;
+
+namespace GeneratorsClass
+{
+    class HauseNumber
+    {
+        
+        static string[] hauseLib = new String[]{"a","b","c","d","e","f"};
+
+        static string GetNumber()
+        {
+            return RandomNumber.Draw(1, 20).ToString();
+        }
+
+        static bool Draw()
+        {
+            if (RandomNumber.Draw(0, 1) == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public static string GetHauseNumber()
+        {
+            StringBuilder str = new StringBuilder();
+
+            str.Append(GetNumber());
+            if (Draw())
+                str.Append(hauseLib[RandomNumber.Draw(0, hauseLib.Length )]);
+            if (Draw())
+                str.Append($"\\{GetNumber()}");
+            return str.ToString();
+        }
+
+    }
+}
