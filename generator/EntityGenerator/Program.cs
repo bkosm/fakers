f6cdf3ee48648c710d3ac2689ef4ddacc39ce75e
Mateusz Kuźniak <mateokuz@gmail.com>
2020-12-02 16:47:38 +0100

    generator v1 - database test

--- generator/EntityGenerator/Program.cs
+++ generator/EntityGenerator/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using GeneratorsClass;
 using ExternalAPI;
+using System.Threading;
 
 namespace EntityGenerator
 {
@@ -15,32 +16,14 @@ namespace EntityGenerator
             {
                 await using var context = new PeopleContext(parsed.GetConnectionString());
 
-                int test = 0;
-               
-                for(int i = 0; i < 2; i++)
-                {
-                    GeneratorsClass.Person temp = new GeneratorsClass.Person();
-                    
-                    var person = personToPerson(temp);
-                    context.People.Add(person);
+                    generate(context, 10 );
 
-                    var adr = setAddress(temp.Street);
-
-                    if (adr.City != null)
-                        context.Addresses.Add(adr);
-
-
-
-                }
-
-                context.SaveChanges();
-
-                await context.Addresses.ForEachAsync(Console.WriteLine);
+               //await context.Addresses.ForEachAsync(Console.WriteLine);
             });
 
             model.Person personToPerson(GeneratorsClass.Person temp)
             {
-                EntityGenerator.model.Person newOne = new EntityGenerator.model.Person();
+                model.Person newOne = new model.Person();
                
 
                 newOne.Sex = temp.Sex;
@@ -57,11 +40,49 @@ namespace EntityGenerator
                 ClearGeoAPI geo = GetDataFromJson.getGeo("", street);          
                 model.Address adr = new model.Address();
                 adr.City = geo.City;
-                adr.Street = street;
-                adr.Voivodeship = geo.State;
+                adr.Street = street +  HouseNumber.GetHouseNumber();
+                adr.Voivodeship = geo.Voivodeship;
+                
                 adr.Location = new String ($"{geo.Latiude}, {geo.Longitude}");
 
                 return adr;
+            }
+
+            void generate(PeopleContext context, int loop)
+            {
+                int i = 0;
+               while(i < loop)
+                {
+                    Thread.Sleep(1000);
+                    GeneratorsClass.Person perosonL = new GeneratorsClass.Person();
+                    
+                    
+
+                    var person = personToPerson(perosonL);
+                    context.People.Add(person);
+
+
+                    var address = setAddress(perosonL.Street);
+                    if (address.City != null)
+                        context.Addresses.Add(address);
+                    else
+                        continue;
+
+
+                    context.SaveChanges();
+
+                    var personAddress = new model.PersonAddress();
+                    if(address.City != null)
+                    {
+                        personAddress.Person= person;
+                        personAddress.Address= address;
+                    }
+                    context.PersonAddresses.Add(personAddress);
+                    Console.WriteLine(++i);
+                    context.SaveChanges();
+                }
+
+               
             }
         }
     }
