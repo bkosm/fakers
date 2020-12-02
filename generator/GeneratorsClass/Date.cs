b5c19cba9e31785d59fbb00eaacd3a8c3d912d50
Mateusz Kuźniak <mateokuz@gmail.com>
2020-12-02 17:10:35 +0100

    RandomNumber class location change

--- generator/GeneratorsClass/Date.cs
+++ generator/GeneratorsClass/Date.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using RandomClass;
+using ExternalAPI;
 
 namespace GeneratorsClass
 {
--- generator/GeneratorsClass/Telephone.cs
+++ generator/GeneratorsClass/Telephone.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using RandomClass;
+using ExternalAPI;
 
 /* 
  * W tej konfiguracji generacja miliona numerów wynosi około 6 sek 
