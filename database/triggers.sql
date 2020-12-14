79ffef65534d5f151ba40af968baf4312fc82b05
bart-kosmala <bartoshmen@gmail.com>
2020-12-14 14:01:00 +0100

    triggers converted to after due to phoenix

--- database/triggers.sql
+++ database/triggers.sql
@@ -9,20 +9,23 @@ BEGIN
     IF NOT NEW.is_primary THEN
 		IF TRUE NOT IN (SELECT is_primary FROM person_address pa 
 						WHERE pa.person_id = NEW.person_id) THEN
-			NEW.is_primary := TRUE;
+			
+			UPDATE person_address
+			SET is_primary = TRUE
+			WHERE person_id = NEW.person_id AND address_id = NEW.address_id;
 		END IF;
 	ELSE
 		UPDATE person_address
 		SET is_primary = FALSE
-		WHERE person_id = NEW.person_id;
+		WHERE person_id = NEW.person_id AND address_id != NEW.address_id;
 	END IF;
 	
-	RETURN NEW;
+	RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER before_insert_update
-BEFORE INSERT OR UPDATE ON person_address
+AFTER INSERT OR UPDATE ON person_address
 FOR EACH ROW
 WHEN (pg_trigger_depth() < 1)
 EXECUTE PROCEDURE before_insert_update_person_address();
@@ -34,20 +37,22 @@ BEGIN
     IF NOT NEW.is_primary THEN
 		IF TRUE NOT IN (SELECT is_primary FROM person_contact pc 
 						WHERE pc.person_id = NEW.person_id) THEN
-			NEW.is_primary := TRUE;
+			UPDATE person_contact
+			SET is_primary = TRUE
+			WHERE person_id = NEW.person_id AND contact_id = NEW.contact_id;
 		END IF;
 	ELSE
 		UPDATE person_contact
 		SET is_primary = FALSE
-		WHERE person_id = NEW.person_id;
+		WHERE person_id = NEW.person_id AND contact_id != NEW.contact_id;
 	END IF;
 	
-	RETURN NEW;
+	RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER before_insert_update
-BEFORE INSERT OR UPDATE ON person_contact
+AFTER INSERT OR UPDATE ON person_contact
 FOR EACH ROW
 WHEN (pg_trigger_depth() < 1)
 EXECUTE PROCEDURE before_insert_update_person_contact();
@@ -59,11 +64,11 @@ BEGIN
     DELETE FROM person_address
 	WHERE person_id = NEW.person_id;
 	
-	RETURN NEW;
+	RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER before_insert
-BEFORE INSERT ON deceased_person
+AFTER INSERT ON deceased_person
 FOR EACH ROW
 EXECUTE PROCEDURE before_insert_deceased_person();
