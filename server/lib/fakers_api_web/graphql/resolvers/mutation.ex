739aab97964b3e3d744dc7d6d267ec7aadcaef15
bart-kosmala <bartoshmen@gmail.com>
2020-12-14 14:30:47 +0100

    mutations ready

--- server/lib/fakers_api_web/graphql/resolvers/mutation.ex
+++ server/lib/fakers_api_web/graphql/resolvers/mutation.ex
@@ -1,2 +1,243 @@
 defmodule FakersApiWeb.GraphQL.Resolvers.Mutation do
+  alias FakersApi.People, as: Context
+
+  def create_person(arguments, _) do
+    case Context.create_person(arguments) do
+      {:ok, person} ->
+        {:ok, person}
+
+      _error ->
+        {:error,
+         "Couldn't create a person from given data. Unique fields might already exist in the database."}
+    end
+  end
+
+  def update_person(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :id)
+
+    case Context.get_person(person_id) do
+      nil ->
+        {:error, "No person found with given id."}
+
+      person ->
+        case Context.update_person(person, arguments) do
+          {:ok, person} ->
+            {:ok, person}
+
+          _error ->
+            {:error,
+             "Couldn't update person using given data. Perhaps a field should not be null?"}
+        end
+    end
+  end
+
+  def create_address(arguments, _) do
+    case Context.create_address(arguments) do
+      {:ok, address} ->
+        {:ok, address}
+
+      _error ->
+        {:error,
+         "Couldn't create an address from given data. Unique fields might already exist in the database."}
+    end
+  end
+
+  def update_address(arguments, _) do
+    {address_id, arguments} = Map.pop(arguments, :id)
+
+    case Context.get_address(address_id) do
+      nil ->
+        {:error, "No person found with given id."}
+
+      address ->
+        case Context.update_address(address, arguments) do
+          {:ok, address} ->
+            {:ok, address}
+
+          _error ->
+            {:error,
+             "Couldn't update address using given data. Perhaps a field should not be null?"}
+        end
+    end
+  end
+
+  def create_contact(arguments, _) do
+    case Context.create_contact(arguments) do
+      {:ok, contact} ->
+        {:ok, contact}
+
+      _error ->
+        {:error,
+         "Couldn't create a contact from given data. Unique fields might already exist in the database."}
+    end
+  end
+
+  def update_contact(arguments, _) do
+    {contact_id, arguments} = Map.pop(arguments, :id)
+
+    case Context.get_contact(contact_id) do
+      nil ->
+        {:error, "No person found with given id."}
+
+      contact ->
+        case Context.update_contact(contact, arguments) do
+          {:ok, contact} ->
+            {:ok, contact}
+
+          _error ->
+            {:error,
+             "Couldn't modify a contact from given data. Perhaps a field should not be null?"}
+        end
+    end
+  end
+
+  def create_deceased_person(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+
+    case Context.get_person(person_id) do
+      nil ->
+        {:error, "No person found with given id."}
+
+      person ->
+        case Context.create_deceased_person(arguments, person) do
+          {:ok, person} ->
+            {:ok, person}
+
+          _error ->
+            {:error, "Couldn't register person as deceased."}
+        end
+    end
+  end
+
+  def remove_deceased_person(%{person_id: person_id}, _) do
+    case Context.get_deceased_person(person_id) do
+      nil ->
+        {:error, "No deceased person with given id found."}
+
+      person ->
+        case Context.delete_deceased_person(person) do
+          {:ok, person} ->
+            {:ok, person}
+
+          _error ->
+            {:error, "Couldn't revive person with given id."}
+        end
+    end
+  end
+
+  def create_person_address(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+    {address_id, arguments} = Map.pop(arguments, :address_id)
+
+    person = Context.get_person(person_id)
+    address = Context.get_address(address_id)
+
+    if is_nil(person) or is_nil(address) do
+      {:error, "Person or address do not exist."}
+    else
+      case Context.create_person_address(arguments, person, address) do
+        {:ok, person_address} ->
+          {:ok, person_address}
+
+        _error ->
+          {:error, "Could not create person and address association. Possible duplicate."}
+      end
+    end
+  end
+
+  def update_person_address(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+    {address_id, arguments} = Map.pop(arguments, :address_id)
+
+    case Context.get_person_address(person_id, address_id) do
+      nil ->
+        {:error, "Given person address association does not exist."}
+
+      person_address ->
+        case Context.update_person_address(arguments, person_address) do
+          {:ok, person_address} ->
+            {:ok, person_address}
+
+          _error ->
+            {:error, "Could not update person address association from given arguments."}
+        end
+    end
+  end
+
+  def remove_person_address(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+    {address_id, _arguments} = Map.pop(arguments, :address_id)
+
+    case Context.get_person_address(person_id, address_id) do
+      nil ->
+        {:error, "Given person address association does not exist."}
+
+      person_address ->
+        case Context.delete_person_address(person_address) do
+          {:ok, person_address} ->
+            {:ok, person_address}
+
+          _error ->
+            {:error, "Could not remove person address association from given arguments."}
+        end
+    end
+  end
+
+  def create_person_contact(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+    {contact_id, arguments} = Map.pop(arguments, :contact_id)
+
+    person = Context.get_person(person_id)
+    contact = Context.get_contact(contact_id)
+
+    if is_nil(person) or is_nil(contact) do
+      {:error, "Person or contact do not exist."}
+    else
+      case Context.create_person_contact(arguments, person, contact) do
+        {:ok, person_contact} ->
+          {:ok, person_contact}
+
+        _error ->
+          {:error, "Could not create person and contact association. Possible duplicate."}
+      end
+    end
+  end
+
+  def update_person_contact(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+    {contact_id, arguments} = Map.pop(arguments, :contact_id)
+
+    case Context.get_person_contact(person_id, contact_id) do
+      nil ->
+        {:error, "Given person contact association does not exist."}
+
+      person_contact ->
+        case Context.update_person_contact(arguments, person_contact) do
+          {:ok, person_contact} ->
+            {:ok, person_contact}
+
+          _error ->
+            {:error, "Could not update person contact association from given arguments."}
+        end
+    end
+  end
+
+  def remove_person_contact(arguments, _) do
+    {person_id, arguments} = Map.pop(arguments, :person_id)
+    {contact_id, _arguments} = Map.pop(arguments, :contact_id)
+
+    case Context.get_person_contact(person_id, contact_id) do
+      nil ->
+        {:error, "Given person contact association does not exist."}
+
+      person_contact ->
+        case Context.delete_person_contact(person_contact) do
+          {:ok, person_contact} ->
+            {:ok, person_contact}
+
+          _error ->
+            {:error, "Could not remove person contact association from given arguments."}
+        end
+    end
+  end
 end
--- server/lib/fakers_api_web/graphql/schema.ex
+++ server/lib/fakers_api_web/graphql/schema.ex
@@ -2,7 +2,7 @@ defmodule FakersApiWeb.GraphQL.Schema do
   use Absinthe.Schema
 
   alias FakersApiWeb.GraphQL.Schema.Types.{Objects, Inputs, Filters}
-  alias FakersApiWeb.GraphQL.Resolvers.{Query}
+  alias FakersApiWeb.GraphQL.Resolvers.{Query, Mutation}
   alias FakersApi.People
 
   import_types(Absinthe.Type.Custom)
@@ -37,6 +37,142 @@ defmodule FakersApiWeb.GraphQL.Schema do
       arg(:filter, :contact_filter)
 
       resolve(&Query.get_contacts/2)
+    end
+  end
+
+  mutation do
+    @desc "Create a new person. Sex must be 'm' or 'f'."
+    field :create_person, :person do
+      arg(:birth_date, non_null(:date))
+      arg(:first_name, non_null(:string))
+      arg(:last_name, non_null(:string))
+      arg(:pesel, non_null(:string))
+      arg(:second_name, :string)
+      arg(:sex, non_null(:string))
+
+      resolve(&Mutation.create_person/2)
+    end
+
+    @desc "Modify an existing person. Sex must be 'm' or 'f'."
+    field :modify_person, :person do
+      arg(:id, non_null(:id))
+      arg(:birth_date, :date)
+      arg(:first_name, :string)
+      arg(:last_name, :string)
+      arg(:pesel, :string)
+      arg(:second_name, :string)
+      arg(:sex, :string)
+
+      resolve(&Mutation.update_person/2)
+    end
+
+    @desc "Create a new address."
+    field :create_address, :address do
+      arg(:city, non_null(:string))
+      arg(:location, non_null(:string))
+      arg(:street, non_null(:string))
+      arg(:voivodeship, non_null(:string))
+
+      resolve(&Mutation.create_address/2)
+    end
+
+    @desc "Modify an existing address."
+    field :modify_address, :address do
+      arg(:id, non_null(:id))
+      arg(:city, :string)
+      arg(:location, :string)
+      arg(:street, :string)
+      arg(:voivodeship, :string)
+
+      resolve(&Mutation.update_address/2)
+    end
+
+    @desc "Create a new contact."
+    field :create_contact, :contact do
+      arg(:email, non_null(:string))
+      arg(:phone_number, :string)
+
+      resolve(&Mutation.create_contact/2)
+    end
+
+    @desc "Modify an existing contact."
+    field :modify_contact, :contact do
+      arg(:id, non_null(:id))
+      arg(:email, :string)
+      arg(:phone_number, :string)
+
+      resolve(&Mutation.update_contact/2)
+    end
+
+    @desc "Register person as deceased."
+    field :register_deceased, :deceased_person do
+      arg(:person_id, non_null(:id))
+      arg(:date_of_death, :date)
+
+      resolve(&Mutation.create_deceased_person/2)
+    end
+
+    @desc "Remove person from the deceased registry."
+    field :remove_deceased, :deceased_person do
+      arg(:person_id, non_null(:id))
+
+      resolve(&Mutation.remove_deceased_person/2)
+    end
+
+    @desc "Create a person address association"
+    field :associate_person_address, :person_address do
+      arg(:person_id, non_null(:id))
+      arg(:address_id, non_null(:id))
+      arg(:assigned, :date)
+      arg(:is_primary, :boolean)
+
+      resolve(&Mutation.create_person_address/2)
+    end
+
+    @desc "Modify a person address association (assigned or isPrimary fields)."
+    field :modify_person_address, :person_address do
+      arg(:person_id, non_null(:id))
+      arg(:address_id, non_null(:id))
+      arg(:assigned, :date)
+      arg(:is_primary, :boolean)
+
+      resolve(&Mutation.update_person_address/2)
+    end
+
+    @desc "Remove a person address association."
+    field :remove_person_address, :person_address do
+      arg(:person_id, non_null(:id))
+      arg(:address_id, non_null(:id))
+
+      resolve(&Mutation.remove_person_address/2)
+    end
+
+    @desc "Create a person contact association"
+    field :associate_person_contact, :person_contact do
+      arg(:person_id, non_null(:id))
+      arg(:contact_id, non_null(:id))
+      arg(:assigned, :date)
+      arg(:is_primary, :boolean)
+
+      resolve(&Mutation.create_person_contact/2)
+    end
+
+    @desc "Modify a person contact association (assigned or isPrimary fields)."
+    field :modify_person_contact, :person_contact do
+      arg(:person_id, non_null(:id))
+      arg(:contact_id, non_null(:id))
+      arg(:assigned, :date)
+      arg(:is_primary, :boolean)
+
+      resolve(&Mutation.update_person_contact/2)
+    end
+
+    @desc "Remove a person contact association."
+    field :remove_person_contact, :person_contact do
+      arg(:person_id, non_null(:id))
+      arg(:contact_id, non_null(:id))
+
+      resolve(&Mutation.remove_person_contact/2)
     end
   end
 
