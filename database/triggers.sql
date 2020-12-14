8808175f83743d01c4c75914441d81bfc9870fd7
bart-kosmala <bartoshmen@gmail.com>
2020-12-14 14:21:34 +0100

    new triggers for deletion

--- database/triggers.sql
+++ database/triggers.sql
@@ -1,10 +1,12 @@
-DROP TRIGGER IF EXISTS before_insert_update ON person_address CASCADE;
-DROP TRIGGER IF EXISTS before_insert_update ON person_contact CASCADE;
-DROP TRIGGER IF EXISTS before_insert ON deceased_person CASCADE;
+DROP TRIGGER IF EXISTS after_insert_update ON person_address CASCADE;
+DROP TRIGGER IF EXISTS after_insert_update ON person_contact CASCADE;
+DROP TRIGGER IF EXISTS after_insert ON deceased_person CASCADE;
+DROP TRIGGER IF EXISTS after_delete ON person_address CASCADE;
+DROP TRIGGER IF EXISTS after_delete ON person_contact CASCADE;
 
 --
 
-CREATE OR REPLACE FUNCTION before_insert_update_person_address() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION after_insert_update_person_address() RETURNS TRIGGER AS $$ -- zapewnij jeden glowny adres osoby po dodaniu
 BEGIN
     IF NOT NEW.is_primary THEN
 		IF TRUE NOT IN (SELECT is_primary FROM person_address pa 
@@ -24,15 +26,33 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_insert_update
+CREATE TRIGGER after_insert_update
 AFTER INSERT OR UPDATE ON person_address
 FOR EACH ROW
 WHEN (pg_trigger_depth() < 1)
-EXECUTE PROCEDURE before_insert_update_person_address();
+EXECUTE PROCEDURE after_insert_update_person_address();
 
 --
 
-CREATE OR REPLACE FUNCTION before_insert_update_person_contact() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION after_delete_person_address() RETURNS TRIGGER AS $$ -- zapewnij jeden glowny adres osoby po usunieciu (najnowszy z pozostalych)
+BEGIN
+    UPDATE person_address
+	SET is_primary = TRUE
+	WHERE person_id = OLD.person_id AND address_id = (SELECT address_id FROM person_address WHERE person_id = OLD.person_id ORDER BY assigned DESC FETCH FIRST 1 ROWS ONLY);
+	
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_delete
+AFTER DELETE ON person_address
+FOR EACH ROW
+WHEN (pg_trigger_depth() < 1)
+EXECUTE PROCEDURE after_delete_person_address();
+
+--
+
+CREATE OR REPLACE FUNCTION after_insert_update_person_contact() RETURNS TRIGGER AS $$ -- zapewnij jeden glowny kontakt osoby po dodaniu
 BEGIN
     IF NOT NEW.is_primary THEN
 		IF TRUE NOT IN (SELECT is_primary FROM person_contact pc 
@@ -51,15 +71,33 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_insert_update
+CREATE TRIGGER after_insert_update
 AFTER INSERT OR UPDATE ON person_contact
 FOR EACH ROW
 WHEN (pg_trigger_depth() < 1)
-EXECUTE PROCEDURE before_insert_update_person_contact();
+EXECUTE PROCEDURE after_insert_update_person_contact();
 
 --
 
-CREATE OR REPLACE FUNCTION before_insert_deceased_person() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION after_delete_person_contact() RETURNS TRIGGER AS $$ -- zapewnij jeden glowny kontakt osoby po usunieciu (najnowszy z pozostalych)
+BEGIN
+    UPDATE person_contact
+	SET is_primary = TRUE
+	WHERE person_id = OLD.person_id AND contact_id = (SELECT contact_id FROM person_contact WHERE person_id = OLD.person_id ORDER BY assigned DESC FETCH FIRST 1 ROWS ONLY);
+	
+	RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_delete
+AFTER DELETE ON person_contact
+FOR EACH ROW
+WHEN (pg_trigger_depth() < 1)
+EXECUTE PROCEDURE after_delete_person_contact();
+
+--
+
+CREATE OR REPLACE FUNCTION after_insert_deceased_person() RETURNS TRIGGER AS $$
 BEGIN
     DELETE FROM person_address
 	WHERE person_id = NEW.person_id;
@@ -68,7 +106,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_insert
+CREATE TRIGGER after_insert
 AFTER INSERT ON deceased_person
 FOR EACH ROW
-EXECUTE PROCEDURE before_insert_deceased_person();
+EXECUTE PROCEDURE after_insert_deceased_person();
